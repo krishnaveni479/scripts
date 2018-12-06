cd35ec4f97e3951a81aa0560b50135f61452cb5f
Siva Kumar Reddy G <gskr.scm@gmail.com>
2018-12-06 18:49:45 +0530

    first commit

--- list_p1.py
+++ list_p1.py
@@ -1,21 +1,11 @@
 #values = ['siva', 'krishna', 'puppy', 'chinnu', 'budi']
 #d = {'Name':'Siva', 'Age': 27}
-#import sys
+import sys
 
-#values = (sys.argv[1],sys.argv[2])
-#for x in values: 
-  #  print(x)
+values = (sys.argv[1],sys.argv[2])
+for x in values: 
+   print(x)
    # f.write("\n")
 #with open(myfile, 'w') as f:
  #   for key, value in a.items():
   #      f.write('%s:%s\n' % (key, value))
-def name_age(namn):
-    dic = {}
-    age = 0
-    for n in range(0, len(namn), 2):
-        dic[namn[n]] = namn[n+1]
-    for i in dic:
-        print i, dic[i]
-        age = age + dic[i]
-    avg_age = age / len(dic)
-    return avg_age
